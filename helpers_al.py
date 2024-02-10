1bd861f774c75aac7acf4967fc22e471460db34e
Harsh Rachalwar <f20210200@hyderabad.bits-pilani.ac.in>
2024-02-10 11:44:34 +0530

    trial

--- helpers_al.py
+++ helpers_al.py
@@ -437,13 +437,12 @@ def query_samples(model, method, data_unlabeled, subset, labeled_set, cycle, arg
             weighted_preds = ner_score*weights[0] + re_score*weights[1]
             weighted_preds = weighted_preds.unsqueeze(1)
             weighted_preds = weighted_preds.cpu().data
-            print(weighted_preds)
-            print(preds)
             weights_list.extend(weighted_preds)
             # all_indices.extend(indices)
 
         
-
+        weights_list = torch.stack(weights_list)
+        weights_list = weights_list.view(-1)
         all_preds = torch.stack(all_preds)
         all_preds = all_preds.view(-1)
         # need to multiply by -1 to be able to use torch.topk 
